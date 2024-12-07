17c940d55d7400354c7132cadaeef7a1264f77ab
Cristian Orozco <cristianorozco.ben@gmail.com>
2024-12-07 12:12:02 -0500

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pricewise",
+  description: "Track product prices effortlessly and save money on your online shopping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main className="max-w-10xl mx-auto">{children}</main>
+      </body>
+    </html>
+  );
+}
